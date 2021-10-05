07f51d5c66c018c705baca15cc1fd1edb60a2d39
Jake Lee <jake.lee@fdmgroup.com>
2021-10-05 11:11:28 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    user = input("Enter username:")
+    password = input("Enter password:")
+    if ((user == "grace" and password == "hopper") or (user == "emma" and password == "haskell")):
+        print("You have successfully logged in!")
+    else:
+        print("Incorrect username or password!")
 
 if __name__ == '__main__':
     main()
